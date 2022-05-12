8d423718a43c41f8bb182b133d51ec0ef4ba28fa
Ander De Abrisqueta Rangel <ander.deabrisqueta@gmail.com>
2022-05-12 22:24:25 +0200

    apk - commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.harenaprojectapp.app',
   appName: 'HarenaProjectApp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
